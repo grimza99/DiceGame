46c50f010ddb4ba5cd680cba0bfe83962fb19602
nerte <grimza@nerteui-MacBookPro.local>
2024-12-30 13:32:08 +0900

    [Feat] function handlClearClick , handlClick
    처음부터 버튼을 누르면 주사위를 초기화 해주는 handleClearClick 함수와,
    던지기 버튼을 누르면 1부터 6중에 랜덤한 숫자를 만들어주는 handleClick 함수 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Dice from "./Dice";
 import Button from "./Button.js";
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(1);
+  const handleClick = () => {
+    setNum(random(6));
+  };
+  function random(n) {
+    return Math.ceil(Math.random() * n);
+  }
+  const handleClearClick = () => {
+    setNum(1);
+  };
   return (
     <div>
-      <Button>던지기</Button>
-      <Button>처음부터</Button>
-      <Dice color="red" num={4}></Dice>
+      <Button onClick={handleClick}>던지기</Button>
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <Dice color="red" num={num}></Dice>
     </div>
   );
 }
