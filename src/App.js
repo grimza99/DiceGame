62debe31034efb5d434ebcbd75196ecf201bc9b2
nerte <grimza@nerteui-MacBookPro.local>
2024-12-30 17:09:31 +0900

    [Feat]create gameHistory useState 게임 기록이 남는 기능추가 gameHistory useState 생성, 던지기 버튼을 누를때마다 gamehistory+ 주사위 숫자를 합치는 새로운 배열 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import Dice from "./Dice";
 import Button from "./Button.js";
 import { useState } from "react";
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
   const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
+
   const handleClick = () => {
-    setNum(random(6));
+    const nextNum = random(6);
+    setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory(...(gameHistory + nextNum));
   };
-  function random(n) {
-    return Math.ceil(Math.random() * n);
-  }
+
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   };
   return (
     <div>
-      <Button onClick={handleClick}>던지기</Button>
-      <Button onClick={handleClearClick}>처음부터</Button>
-      <Dice color="red" num={num}></Dice>
+      <div>
+        <Button onClick={handleClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <div>
+        <h2>나</h2>
+        <Dice color="blue" num={num}></Dice>
+        <h2>상대</h2>
+        <Dice color="red" num={num}></Dice>
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        <p>{gameHistory.join(", ")}</p>
+      </div>
     </div>
   );
 }
